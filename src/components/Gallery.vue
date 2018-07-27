f31f3224765726ed579e5bf6b29caa9ea380b222
Matt <mpau548@aucklanduni.ac.nz>
2018-07-28 02:04:34 +0800

    Updated events page styles and content. Started work on the gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="hello">
-    <div class="gallery-container">
-      <h1>Image Gallery! 🖼️</h1>
-    </div>
+    <vue-instagram token="4129170661.1677ed0.b434535d2a95436889c4c50b7a782e90" username="kiwikarateka" :count="6" class="gallery-container">
+      <template slot="feeds" scope="props">
+        <a :href="props.feed.link" target="_blank">
+          <div class="card-image">
+            <img :src="props.feed.images.low_resolution.url" alt="Instagram Image">
+          </div>
+        </a>
+      </template>
+    </vue-instagram>
   </div>
 </template>
 
 <script>
+import VueInstagram from 'vue-instagram'
+
 export default {
   name: 'Gallery',
-  props: {
-    msg: String
-  }
+  components: {
+    VueInstagram
+  },
 }
 </script>
 
@@ -21,7 +29,17 @@ export default {
   .gallery-container {
     width: 100%;
     height: 400px;
-    background-color: var(--tuatara-light);
+    display: grid;
+    grid-template-columns: repeat(3, auto);
+    grid-gap: 10px;
+  }
+
+  img {
+    width: 100%;
+    border-radius: 15px;
+  }
+
+  .card {    
   }
 
 
--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,22 +1,104 @@
+
 <template>
-  <div class="home">
-    <h1>Coming soon:</h1>
-    <ul>
-      <li>Karate Kamp</li>
-      <li>Courses and Careers Day</li>
-      <li>October Gasshuku</li>
-    </ul>
+  <div class="event-container">
+    <div class="">
+      <h2 class="event-title">Coming soon:</h2>
+      <p class="page-desc">
+        This page lists upcoming club events. There is no gurauntee it is up to date.
+        This page was last updated on 28/06/2018. If you have any questions about upcoming events contact us.
+        </p>
+      </div>
+    <div v-for="event in eventList" :key="event.name" class="event">
+      <h2 class="event-title">{{event.name}}</h2>
+      <img :src="event.img" alt="Event image" v-if="event.img" class="event-img">
+      <div class="event-text"><b>Date:</b> {{event.date}}</div>
+      <div class="event-text"><b>Location:</b> {{event.location}}</div>
+      <div class="event-text">{{event.description}}</div>
+      <div class="event-text">For more information follow <a :href="event.link">this link</a></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-
 
 export default {
   name: 'Events',
   components: {
+  },
+  data: function() {
+    return {
+      eventList: [
+        {
+          name: 'Karate Kamp 2018',
+          img: '/logo.png',
+          date: 'Sem 2 Mid Semester Break',
+          location: 'Undecided',
+          description: 'The third annual UoA Karate Kamp. Fun, games and Daily Practice.',
+          link: ''
+        },
+        {
+          name: 'Courses & Careers Day 2018',
+          date: 'August 25',
+          location: 'UoA Campus',
+          description: 'The club will be performing at Courses and Careers Day',
+          link: ''
+        },
+        {
+          name: '2018 National Gasshuku',
+          img: '/logo.png',
+          date: 'October 20th-21st',
+          location: 'Wellington',
+          description: 'Ernie Sensei, 8th dan from England, will be coming to Wellington to train.',
+          link: 'www.karate.org.nz'
+        },
+        {
+          name: '2018 Taira Sensei Seminar',
+          date: 'October 27th-28th',
+          location: 'Auckland',
+          description: 'Taira Masaji Sensei, 9th dan from Okinawa, will be coming to Auckland to host a seminar.',
+          link: ''
+        },
+      ]
+    }
   }
 }
 </script>
+
+<style scoped>
+.event-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(auto, 600px));
+  grid-auto-rows: minmax(200px, auto);
+  grid-gap: 30px;
+  grid-auto-flow: dense;
+  justify-content: center;
+}
+
+.event {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 5px;
+  grid-auto-flow: dense;
+}
+
+.event-title {
+  grid-area: 1/1/2/3;
+  justify-self: center;
+  text-align: center;
+}
+
+.event-img {
+  justify-self: center;
+  grid-area: 2/2/6/3;
+  max-height: 100%;
+  max-width: 150px;
+}
+
+.event-text, .page-desc {
+  padding-left: 20px;
+}
+
+.page-desc {
+
+}
+</style>
