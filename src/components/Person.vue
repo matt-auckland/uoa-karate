e2ebe7965d0a5037da9ddebd625b1ae5654180d9
Matt <mpau548@aucklanduni.ac.nz>
2018-08-04 02:39:45 +0800

    added json file for people. updaed the person view so we can deal with people as objects easier

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -3,7 +3,12 @@
     <img :src="person.pic" />
     <div class="name">{{person.name}}</div>
     <div class="position">{{person.position}}</div>
-    <div class="bio">{{person.bio}}</div>
+    <div class="bio">
+      {{person.bio}} {{person.name | firstName | sensei(person.role)}} holds 
+      <span v-for="style in person.styles" :key="style.name">
+        {{ style.grade | grade(style.name, true)}}{{(person.styles.indexOf(style) === person.styles.length - 1) ? '.' : (person.styles.indexOf(style) === person.styles.length - 2) ? ' and ' : ', '}}
+      </span>
+    </div>
 
   </div>
 </template>
@@ -13,6 +18,42 @@ export default {
   name: 'Person',
   props: {
     person: Object
+  },
+  filters: {
+    grade: function (grade, style = '', prefix) {
+      let determiner = 'a '
+      let danGrade = false
+      let suffix = 'th '
+      if (style)
+        style = ' in ' + style
+      if( grade < 0 ) {
+        danGrade = true
+        grade *= -1
+      } else if (grade === 0 || !grade) {
+        return 'ungraded' + style
+      }
+      switch (grade) {
+        case 1: suffix = 'st '
+                break
+        case 2: suffix = 'nd '
+                break
+        case 3: suffix = 'rd '
+                break
+        case 8: determiner = 'an '
+                break
+      }
+      if (!prefix)
+        determiner = ''
+      return determiner + grade + suffix + (danGrade ? 'Dan ' : 'Kyu ') + style
+    },
+    firstName: function (name) {
+      return name.substr(0, name.indexOf(' ')) || name
+    },
+    sensei: function (name, role = 'instructor') {
+      if (role === 'instructor')
+        return name + ' Sensei'
+      return name
+    }
   }
 }
 </script>
