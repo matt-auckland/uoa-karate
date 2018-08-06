43e080661681feb3fdf19def43bbe2ef7e76ad87
Matt <mpau548@aucklanduni.ac.nz>
2018-08-07 00:14:48 +1200

    made all people hae a photo by default

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="person">
-    <img :src="person.pic" />
+    <img :src="person.pic || '/logo.png'" />
     <div class="name">{{person.name}}</div>
     <div class="position">{{person.position}}</div>
     <div class="bio">
