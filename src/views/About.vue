93703deb5a04c5e601e2a118026ee46e7d439762
Matt <mpau548@aucklanduni.ac.nz>
2018-08-06 18:54:23 +1200

    Tweaked About page layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="about-container">
-    <div class="container">
+    <div class="header">
 
       <h1>About the Club</h1>
     
       <p>The club was founded in 2003 by Ewan Tempero Sensei and Tom Davies Sensei. The two met in 2002 when Ewan Sensei ran some beginners' classes in order to create a club. At the time Ewan Sensei had a Nidan in Goju Ryu and Tom Sensei had Shodan ranks in Zen Do Kai Karate Kickboxing, Shin Do Kempo Karate and a Nidan in Yoshukai Karate.</p>
 
       <p>The club is affiliated with the <a href="https://iogkf.com" target="_blank" rel="noopener noreferrer">International Okinawan Goju Ryu Federation (IOGKF)</a>, an international body dedicated to preserving the art of the Okinawan Goju-Ryu Karate, as well as the New Zealand IOGKF affiliate, the <a href="https://karate.org.nz" target="_blank" rel="noopener noreferrer">IOGKFNZ</a>. This gives club members access to expert instructions both nationally and internationally. </p>
-
+    </div>
+    <TrainingSchedule class="schedule" />
+    <FeesTable class="fees" />
+    <div class="people-container">
       <h2>Instructors</h2>
       <div class="person-container">
         <Person v-for="instructor in ClubMembers.filter(person => person.role === 'instructor')" :key="instructor.name" :person="instructor"/>
@@ -17,8 +20,6 @@
         <Person v-for="execMember in ClubMembers.filter(person => person.role === 'exec')" :key="execMember.name" :person="execMember"/>
       </div>
     </div>
-    <TrainingSchedule class="schedule" />
-    <FeesTable class="fees" />
   </div>
 </template>
 
@@ -56,7 +57,7 @@ export default {
   justify-content: center;
 }
 
-.container {
+.header, .people-container {
   grid-column-start: 1;
   grid-column-end: -1;
   width: 100%;
