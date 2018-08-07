cb2499cc809b2db62ea06deca507fdd5d97b4f5c
Matt <mpau548@aucklanduni.ac.nz>
2018-08-07 23:56:57 +1200

    Updated link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
       <router-link to="/about">About the club</router-link> |
       <router-link to="/goju-ryu">About&nbsp;Goju&nbsp;Ryu&nbsp;Karate</router-link> |
       <router-link to="/events">Club&nbsp;Events</router-link> |
-      <a href="http://uoa-karate.club/wiki" target="_blank" rel="noopener noreferrer">Karate&nbsp;Wiki</a> |
+      <a href="https://wiki.uoa-karate.club/" target="_blank" rel="noopener noreferrer">Karate&nbsp;Wiki</a> |
       <a href="https://www.facebook.com/UoAKarate/" target="_blank" rel="noopener noreferrer"><img src="/facebook.svg" alt="[FB]" class="social"></a> |
       <a href="https://www.instagram.com/uoa_karate/" target="_blank" rel="noopener noreferrer"><img src="/instagram.svg" alt="[IG]" class="social"></a>  
     </div>
