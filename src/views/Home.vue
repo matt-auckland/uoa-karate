a377db8ddeec1436b573a0ceb93e9dbc32e32a0d
Matt <mpau548@aucklanduni.ac.nz>
2018-10-16 00:16:10 +1300

    Tweaked home layout a bit more

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -50,7 +50,7 @@ export default {
   .container {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(auto, 700px));
-    grid-auto-rows: minmax(100px, auto);
+    grid-auto-rows: minmax(150px, auto);
   
     /* grid-template-columns: repeat(auto-fit, minmax(100px, 1fr)); */
     /* grid-auto-rows: minmax(100px, 200px); */
@@ -62,33 +62,21 @@ export default {
   }
 
   .home-intro {
-    /* grid-column-end: span 6; */
-    /* grid-row-end: span 1; */
   }
 
   .upcoming-events {
-    /* grid-column-end: span 6; */
-    grid-row-end: span 3;
+    grid-row-end: span 2;
   }
 
   .schedule {
-    /* grid-column-end: span 6; */
-    /* grid-row-end: span 1; */
   }
 
   .fees {
-    /* grid-column-end: span 6; */
-    grid-row-end: span 2;
   }
   .clubmap {
-    /* grid-column-end: span 6; */
-    /* grid-row-end: span 3; */
-    /* margin: 30px 0 10px 0; */
   }
 
   .contact-form {
-    /* grid-column-end: span 6; */
-    /* grid-row-end: span 3; */
   }
   .quote-display {
     grid-area: auto / 1/ auto/ -1;
