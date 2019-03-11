f57a15122cb169987ce51cbd5ebc1f1fc6f3eedf
Matt <mpau548@aucklanduni.ac.nz>
2019-03-11 19:26:13 +1300

    Removed karate page, now we link to wiki. Added useful links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,51 @@
 <template>
-  <div class="header">                                                
-    <router-link to="/"><img src="/logo.png" alt="IOGKF Logo"></router-link> 
+  <div class="header">
+    <router-link to="/"><img
+        src="/logo.png"
+        alt="IOGKF Logo"
+      ></router-link>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About the club</router-link> |
-      <router-link to="/goju-ryu">About&nbsp;Goju&nbsp;Ryu&nbsp;Karate</router-link> |
+      <a
+        href="https://wiki.uoa-karate.club/wiki/Goju_Ryu"
+        target="_blank"
+      >About&nbsp;Goju&nbsp;Ryu&nbsp;Karate</a> |
       <router-link to="/events">Club&nbsp;Events</router-link> |
       <router-link to="/tester">Knowledge&nbsp;Tester</router-link> |
-      <a href="https://wiki.uoa-karate.club/" target="_blank" rel="noopener noreferrer">Karate&nbsp;Wiki</a> |
-      <a href="https://www.facebook.com/UoAKarate/" target="_blank" rel="noopener noreferrer"><img src="/facebook.svg" alt="[FB]" class="social"></a> |
-      <a href="https://www.instagram.com/uoa_karate/" target="_blank" rel="noopener noreferrer"><img src="/instagram.svg" alt="[IG]" class="social"></a>  
+      <a
+        href="https://wiki.uoa-karate.club/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >Karate&nbsp;Wiki</a> |
+      <router-link to="/useful-links">Useful&nbsp;Links</router-link> |
+      <a
+        href="https://www.facebook.com/UoAKarate/"
+        target="_blank"
+        rel="noopener noreferrer"
+      ><img
+          src="/facebook.svg"
+          alt="[FB]"
+          class="social"
+        ></a> |
+      <a
+        href="https://www.instagram.com/uoa_karate/"
+        target="_blank"
+        rel="noopener noreferrer"
+      ><img
+          src="/instagram.svg"
+          alt="[IG]"
+          class="social"
+        ></a>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header',
-  props: {
-  }
-}
+  name: "Header",
+  props: {}
+};
 </script>
 
 
@@ -67,18 +93,17 @@ img:hover {
 
 @media (max-width: 500px) {
   .social {
-    width: calc(32px * 0.875)
+    width: calc(32px * 0.875);
   }
 }
 
 @media (max-width: 350px) {
   .social {
-    width: calc(32px * 0.75)
+    width: calc(32px * 0.75);
   }
 }
 
 .social:hover {
   width: 36px;
 }
-
 </style>
---
+++ src/views/LinkDirectory.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>A collection of useful forms and websites</h1>
+    <div class="container">
+      <ul>
+        <li>
+          <a
+            href="https://goo.gl/forms/YMUAxBg9q3fgG0VB3"
+            target="_blank"
+          >Club Reimbursement Form</a>
+        </li>
+        <li><a
+            href="https://goo.gl/forms/sTq5WHgxXTOSmMx42"
+            target="_blank"
+          >Club Sign Up Sheet</a></li>
+        <li><a
+            href="http://qualitas.cs.auckland.ac.nz/iogkfnz/"
+            target="_blank"
+          >Club Roll Call</a></li>
+        <li><a
+            href="http://karate.org.nz/"
+            target="_blank"
+          >IOGKFNZ's Website</a></li>
+        <li><a
+            href=""
+            target="_blank"
+          ></a></li>
+        <li><a
+            href=""
+            target="_blank"
+          ></a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LinkDirectory",
+  components: {}
+};
+</script>
+
+<style scoped>
+.container {
+  text-align: left;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  grid-template-columns: repeat(2, minmax(300px, 1fr));
+}
+
+.container p {
+  max-width: 600px;
+  align-self: flex-start;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
+
