3c0fe7349fdbe459091f1c228b975130f6432055
Matt <mpau548@aucklanduni.ac.nz>
2020-02-06 16:28:08 +1300

    Header now has a home link. Updated CSS and template to be simpler

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,59 +1,38 @@
 <template>
   <div class="header">
-    <router-link to="/"><img
-        id="logo"
-        src="/logo.png"
-        alt="IOGKF Logo"
-      ></router-link>
-    <div id="nav">
-      <router-link
-        to="/about"
-        class="col-2"
-      >About</router-link>
-      <router-link
-        to="/events"
-        class="col-2"
-      >Events</router-link>
-      <router-link
-        to="/tester"
-        class="col-2"
-      >Quiz</router-link>
-      <a
-        href="https://wiki.uoa-karate.club/"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="col-2"
-      >Wiki</a>
-      <router-link
-        to="/useful-links"
-        class="col-4"
-      >Useful&nbsp;Links</router-link>
-      <!-- <a
+    <router-link to="/" id="logo-link"
+      ><img id="logo" src="/logo.png" alt="IOGKF Logo"
+    /></router-link>
+
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/events">Events</router-link>
+    <router-link to="/tester">Quiz</router-link>
+    <a
+      href="https://wiki.uoa-karate.club/"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Wiki</a
+    >
+    <router-link to="/useful-links">Useful&nbsp;Links</router-link>
+    <!-- <a
         href="https://en.wikipedia.org/wiki/G%C5%8Dj%C5%AB-ry%C5%AB"
         target="_blank"
         class="external-link col-5"
       >About&nbsp;Goju&nbsp;Ryu</a> -->
-      <a
-        href="https://www.facebook.com/UoAKarate/"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="col-2"
-      ><img
-          src="/facebook.svg"
-          alt="[FB]"
-          class="social"
-        ></a>
-      <a
-        href="https://www.instagram.com/uoa_karate/"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="col-2"
-      ><img
-          src="/instagram.svg"
-          alt="[IG]"
-          class="social"
-        ></a>
-    </div>
+    <a
+      href="https://www.facebook.com/UoAKarate/"
+      target="_blank"
+      rel="noopener noreferrer"
+      ><img src="/facebook.svg" alt="[FB]" class="social"
+    /></a>
+
+    <a
+      href="https://www.instagram.com/uoa_karate/"
+      target="_blank"
+      rel="noopener noreferrer"
+      ><img src="/instagram.svg" alt="[IG]" class="social"
+    /></a>
   </div>
 </template>
 
@@ -64,7 +43,6 @@ export default {
 };
 </script>
 
-
 <style scoped>
 .external-link:after {
   content: "";
@@ -72,44 +50,18 @@ export default {
     url("data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 width=%2212%22 height=%2212%22%3E %3Cpath fill=%22%23fff%22 stroke=%22%2336c%22 d=%22M1.5 4.518h5.982V10.5H1.5z%22/%3E %3Cpath fill=%22%2336c%22 d=%22M5.765 1H11v5.39L9.427 7.937l-1.31-1.31L5.393 9.35l-2.69-2.688 2.81-2.808L4.2 2.544z%22/%3E %3Cpath fill=%22%23fff%22 d=%22M9.995 2.004l.022 4.885L8.2 5.07 5.32 7.95 4.09 6.723l2.882-2.88-1.85-1.852z%22/%3E %3C/svg%3E");
 }
 
-#nav {
-  max-width: 500px;
-  margin: 30px 0 0 0;
-  width: 100%;
-  min-width: 320px;
-  display: grid;
-  justify-content: center;
-  grid-gap: 10px;
-  grid-template-columns: repeat(auto-fit, 20px);
-}
-
-#nav > * {
-  /* margin: 0 15px; */
-}
-
 .header {
   text-align: left;
-  align-items: center;
+  align-items: flex-end;
   margin: 20px 0 40px;
   display: flex;
-  align-items: flex-end;
+  justify-items: center;
   justify-content: center;
   flex-wrap: wrap;
 }
 
-.col-1 {
-  grid-column: auto / span 1;
-}
-.col-2 {
-  grid-column: auto / span 2;
-}
-
-.col-4 {
-  grid-column: auto / span 4;
-}
-
-.col-5 {
-  grid-column: auto / span 5;
+.header > * {
+  margin: 0 5px;
 }
 
 h3 {
@@ -150,5 +102,12 @@ a {
 
 .social:hover {
   border: 1px solid var(--persian-red);
+}
+
+@media (max-width: 574px) {
+  #logo-link {
+    width: 100%;
+    text-align: center;
+  }
 }
 </style>
