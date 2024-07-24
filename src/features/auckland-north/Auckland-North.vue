3090db7e70aaacc8a1ffd7d1736d6ae1ce885f82
matt-auckland <mathewp.94@gmail.com>
2024-07-24 23:18:08 +1200

    Overhaul Auckland North page with better descriptions/introductions and introduce the instructor

--- src/features/auckland-north/Auckland-North.vue
+++ src/features/auckland-north/Auckland-North.vue
@@ -3,12 +3,28 @@
     <div class="container">
       <HeroImage class="hero-image" source="/group-photo.jpg" heading="Auckland North Karate Club"
                  :paragraph="paragraphText" />
+      <div class="flavour-text">
+        <h2>What is Goju Ryu Karate?</h2>
+        <p>Goju Ryu Karate is a martial art that originated from Okinawa, Japan. Goju Ryu Karate utilises fluid,
+          circular movements and teaches effective close-distance fighting, fast strikes and takedowns. With an
+          emphasises both strength and flexibility, Goju Ryu enhances physical fitness while also building mental
+          discipline and self-confidence.</p>
+        <p>Renowned for its practical self-defense techniques, Goju Ryu is perfect for both beginners and experienced
+          martial artists of all ages, join us today to learn to protect yourself and your loved ones with confidence.
+        </p>
+      </div>
+      <div class="richard">
+        <h2>Meet your instructor:</h2>
+        <MemberCard :member="richard" :wide="true" />
+      </div>
+      <div>
+      </div>
       <TrainingSchedule :schedule="schedule" class="schedule" />
       <FeesTable :feeTableData="feeData" class="fees" />
     </div>
     <ClubMap :mapUrl="mapUrl" class="clubmap" />
     <div class="contact-form">
-      <h1>Contact Auckland North Goju Ryu</h1>
+      <h2>Contact Auckland North Goju Ryu</h2>
       <p style="font-size: 18px">
         Email us at <a href="mailto:an.gojuryu.karate@gmail.com">an.gojuryu.karate@gmail.com</a>
         <br>
@@ -23,11 +39,14 @@
 </template>
 
 <script>
+import MemberCard from "@/components/member-card.vue";
 import TrainingSchedule from "@/components/TrainingSchedule.vue";
 import FeesTable from "@/components/fees-table/FeesTable.vue";
 import ClubMap from "@/components/ClubMap.vue";
 import HeroImage from "../../components/HeroImage.vue";
 // import ContactForm from "@/components/ContactForm.vue";
+import Utils from "@/libs/utils.js";
+
 
 export default {
   name: "home-page",
@@ -35,7 +54,8 @@ export default {
     ClubMap,
     TrainingSchedule,
     FeesTable,
-    HeroImage
+    HeroImage,
+    MemberCard
   },
   data: function () {
     return {
@@ -68,7 +88,16 @@ export default {
           notes: 'Gradings occur twice a year',
         },
       ],
-      paragraphText: "Goju Ryu is a traditional martial art which focuses on mental and physical improvement.Beginners and experienced martial artists are welcome."
+      paragraphText: "Goju Ryu Karate is a martial art that originated from Okinawa, Japan. Goju Ryu Karate utilises fluid, circular movements and teaches effective close-distance fighting, fast strikes and takedowns. With an emphasises both strength and flexibility, Goju Ryu enhances physical fitness, builds mental discipline, as well as self-confidence.",
+      paragraphText: "We're a Goju Ryu Karate dojo based in Sunnynook on the North Shore. We are open to beginners and experienced martial artists alike. Families and children (12+) are also welcome.",
+      // paragraphText: "Goju Ryu is a traditional martial art which focuses on mental and physical improvement. Beginners and experienced martial artists are welcome.",
+      richard: {
+        name: "Sensei Richard Ly",
+        danGrade: true,
+        rank: 4,
+        desc: `Sensei Richard has over ${Utils.calculateYears('03/01/2008')} years of Goju Ryu experience, and is the founder and head instructor of Auckland North.`,
+        imgSrc: "/img/sensei_richard.png"
+      },
     };
   }
 };
@@ -86,6 +115,14 @@ content {
   grid-row-gap: 40px;
 }
 
+.richard {
+  grid-area: richard;
+}
+
+.flavour-text {
+  grid-area: flavour;
+}
+
 .container {
   display: grid;
   grid-area: content;
@@ -94,14 +131,13 @@ content {
   grid-template-columns: 1fr 1fr 2fr;
   grid-template-areas:
     "hero hero hero"
+    "flavour flavour richard"
     "schedule schedule  fees"
     /* "fees events events"; */
 }
 
 @media (max-width: 981px) {
   content {
-    text-align: center;
-
     grid-template-areas:
       "content content"
       "map map"
@@ -112,12 +148,27 @@ content {
     grid-template-columns: 1fr 1fr;
     grid-template-areas:
       "hero hero"
+      "flavour richard"
       "schedule schedule"
       "fees fees";
   }
 
   .events {
     display: none;
+  }
+}
+
+@media (max-width: 600px) {
+  content {
+    text-align: center;
+  }
+  .container {
+    grid-template-columns: 1fr;
+    grid-template-areas: "hero"
+      "flavour"
+      "richard"
+      "schedule"
+      "fees";
   }
 }
 
@@ -175,4 +226,3 @@ content {
   grid-area: events;
 }
 </style>
-
