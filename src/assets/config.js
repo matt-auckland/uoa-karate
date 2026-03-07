a5b8d499494c783dcf999b063295c744b638f110
Mat <matt-auckland@users.noreply.github.com>
2026-03-07 18:17:09 +1300

    fix typo in training locatio for wednesday (wrong floor)

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,53 +1,61 @@
 export const TRAINING_STATUS = {
-  ACTIVE: 'ACTIVE',
-  HIDDEN: 'HIDDEN',
-  POSTPONED: 'POSTPONED'
-}
+  ACTIVE: "ACTIVE",
+  HIDDEN: "HIDDEN",
+  POSTPONED: "POSTPONED",
+};
 
 export default {
-  "bannerText": {
+  bannerText: {
     //"text": "Our 'O-Week' social event is on Wednesday 4th March 6pm-8pm at the Combat Sports Studio in the Rec Center."
   },
-  "schedule": [
+  schedule: [
     {
-      "index": 1,
-      "day": "Tuesdays",
-      "startTime": "6pm",
-      "finishTime": "7.30pm",
-      "location": "Rec Centre (Hiwa) Mind and Body Studio. 2nd floor, University of Auckland",
-      "notes": "",
-      "status": TRAINING_STATUS.HIDDEN,
+      index: 1,
+      day: "Tuesdays",
+      startTime: "6pm",
+      finishTime: "7.30pm",
+      location:
+        "Rec Centre (Hiwa) Mind and Body Studio. 2nd floor, University of Auckland",
+      notes: "",
+      status: TRAINING_STATUS.HIDDEN,
     },
     {
-      "index": 2,
-      "day": "Wednesdays",
-      "startTime": "6.00pm",
-      "finishTime": "7.30pm",
-      "location": "Rec Centre (Hiwa) Combat Sports Studio. 2nd floor, University of Auckland",
-      "notes": "Attendees should meet in the Rec Centre lobby before training starts",
-      "status": TRAINING_STATUS.ACTIVE,
-      "tempNotes": "Wednesday training is currently postponed until January 14th."
+      index: 2,
+      day: "Wednesdays",
+      startTime: "6.00pm",
+      finishTime: "7.30pm",
+      location:
+        "Rec Centre (Hiwa) Combat Sports Studio. 1st floor, University of Auckland",
+      notes:
+        "Attendees should meet in the Rec Centre lobby before training starts",
+      status: TRAINING_STATUS.ACTIVE,
+      tempNotes:
+        "Wednesday training is currently postponed until January 14th.",
     },
     {
-      "index": 3,
-      "day": "Fridays",
-      "startTime": "6.00pm",
-      "finishTime": "7.30pm",
-      "location": "Room 155, Building 303, University of Auckland, 38 Princes Street",
-      "notes": "For graded members, 9th kyu (Blue belt) and above only.",
-      "tempNotes": "Friday training is currently postponed until further notice.",
-      "tempLocation": "Location to be confirmed",
-      "status": TRAINING_STATUS.HIDDEN
+      index: 3,
+      day: "Fridays",
+      startTime: "6.00pm",
+      finishTime: "7.30pm",
+      location:
+        "Room 155, Building 303, University of Auckland, 38 Princes Street",
+      notes: "For graded members, 9th kyu (Blue belt) and above only.",
+      tempNotes: "Friday training is currently postponed until further notice.",
+      tempLocation: "Location to be confirmed",
+      status: TRAINING_STATUS.HIDDEN,
     },
     {
-      "index": 4,
-      "day": "Sundays",
-      "startTime": "10am",
-      "finishTime": "12pm",
-      "location": "Rec Centre (Hiwa) Combat Sports Studio. 1st floor, University of Auckland",
-      "notes": "Attendees should meet in the Rec Centre lobby before training starts",
-      "status": TRAINING_STATUS.POSTPONED,
-      "tempNotes": "Sunday training is currently postponed until March 15th, there is no training on March 8th"
-    }
-  ]
-}
+      index: 4,
+      day: "Sundays",
+      startTime: "10am",
+      finishTime: "12pm",
+      location:
+        "Rec Centre (Hiwa) Combat Sports Studio. 1st floor, University of Auckland",
+      notes:
+        "Attendees should meet in the Rec Centre lobby before training starts",
+      status: TRAINING_STATUS.POSTPONED,
+      tempNotes:
+        "Sunday training is currently postponed until March 15th, there is no training on March 8th",
+    },
+  ],
+};
